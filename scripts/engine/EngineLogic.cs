c81e6d464a67e32306e6f56833fff8f73b981d87
K7 <k7.tapegames@gmail.com>
2025-12-11 17:45:10 +0100

    base du gameplay roulette

---
+++ scripts/engine/EngineLogic.cs
@@ -0,0 +1,75 @@
+using Godot;
+using System;
+
+public partial class EngineLogic : Node
+{
+    private int Gold = 0;
+    private int BoldPoints = 0;
+
+    [Export] private int CylinderSize = 4;
+    private Godot.Collections.Array<BulletType> Cylinder = [];
+    private int ShotsTaken = 0;
+    private float BaseCombo = 1.0f;
+
+    public override void _Ready() {
+        LoadRevolver();
+    }
+
+    public override void _PhysicsProcess(double delta) {
+        if (Input.IsActionJustPressed("shoot")) {
+            Shoot();
+        }
+
+        if (Input.IsActionJustPressed("reload")) {
+            LoadRevolver();
+        }
+    }
+
+    private void LoadRevolver() {
+        Cylinder.Resize(CylinderSize);
+        Cylinder.Fill(BulletType.Blank);
+        LoadBullet(BulletType.Death);
+        GD.Print(Cylinder);
+    }
+
+    private void LoadBullet(BulletType BulletType) {
+        Random rnd = new Random();
+        int Bullet  = rnd.Next(0, CylinderSize);
+        Cylinder[Bullet] = BulletType;
+    }
+
+    private void Shoot() {
+        if (ShotsTaken < CylinderSize) {
+            BulletHandler(Cylinder[ShotsTaken]);
+        }
+    }
+
+    private void Die() {
+        CalculateCombo();
+        LoadRevolver();
+        BaseCombo = 1.0f;
+        ShotsTaken = 0;
+    }
+
+    private void BulletHandler(BulletType bullet) {
+        if (bullet == BulletType.Death) {
+            GD.Print("die");
+            Die();
+        } else if (bullet == BulletType.Blank)
+        {
+            BaseCombo = BaseCombo + 0.1f;
+            ShotsTaken++;
+        }
+    }
+
+    private void CalculateCombo() {
+        float Combo = BaseCombo * 100;
+        int NewGold =  ShotsTaken * (int) Combo;
+        int NewBoldPoints =  ShotsTaken * (int) Combo;
+
+        Gold = Gold + NewGold;
+        BoldPoints = BoldPoints + NewBoldPoints;
+        GD.Print(Combo);
+        GD.Print(BoldPoints);
+    }
+}
---
+++ scripts/enum/BulletType.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+enum BulletType
+{
+    Death,
+    Blank
+}
