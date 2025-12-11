9ce05d3f20cc0a1f6acc49fa09818b80f1d23f7a
Sensouh <nico412003@gmail.com>
2025-12-11 17:32:55 +0100

    Add MainMenuButton

--- scripts/uis/button/DynamiqueButton.gd
+++ scripts/uis/button/DynamiqueButton.gd
@@ -1,9 +1,11 @@
 @tool
 extends DynamiqueText
 
-
+#@export var : String
+signal cliked(name : String)
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		print("Left mouse button was pressed!")
+		
+		cliked.emit(self.name)
 
